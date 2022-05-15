781324f25dc69b049da626d291fe04a32880a28d
饭饭 <76698242@qq.com>
2022-05-15 17:09:34 +0800

    feat: init moyu-blog hello world !!!

---
+++ moyu-blog-start/src/test/java/org/moyu/blog/start/ApplicationTests.java
@@ -0,0 +1,18 @@
+package org.moyu.blog.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void Test() {
+        System.out.println("moyu-blog test !!!");
+    }
+
+}
