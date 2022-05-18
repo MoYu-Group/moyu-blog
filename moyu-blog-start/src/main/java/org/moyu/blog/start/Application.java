3897a1544183bca25436bbb0785b589c2eb78a58
饭饭 <76698242@qq.com>
2022-05-19 01:16:33 +0800

    feat: 集成 moyu-framework 快速开发框架！

--- moyu-blog-start/src/main/java/org/moyu/blog/start/Application.java
+++ moyu-blog-start/src/main/java/org/moyu/blog/start/Application.java
@@ -3,7 +3,15 @@ package org.moyu.blog.start;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * 应用启动类
+ * <p>
+ * Created by fanfan on 2022/05/19.
+ */
+@SpringBootApplication(
+        //扫描MoYu框架中的类
+        scanBasePackages = {"org.moyu", "io.github.moyugroup"}
+)
 public class Application {
 
     public static void main(String[] args) {
